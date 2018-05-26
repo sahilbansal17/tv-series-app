3044eca81df4d1f25a3ffa417ae068ce8690487e
Sahil Bansal <shlbnsl843@gmail.com>
2018-05-26 15:13:45 +0530

    new component Series added, container component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import Intro from '../Intro'
+import Series from '../../containers/Series'
 import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-  state = {
-    series: []
-  }
-  
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then((response) => response.json())
-      .then(json => this.setState({ series: json}))
-  }
-  
   render() {
     return (
       <div className="App">
@@ -21,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
