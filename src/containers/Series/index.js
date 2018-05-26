8f2fb05ebd85fd35e6d9c7423288dbe6c045b4ad
Sahil Bansal <shlbnsl843@gmail.com>
2018-05-26 15:22:03 +0530

    SeriesList presentational component, lists and keys

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import SeriesList from '../../components/SeriesList'
 class Series extends Component {
   state = {
     series: []
@@ -15,6 +15,7 @@ class Series extends Component {
     return (
       <div>
         The length of series array - {this.state.series.length}
+        <SeriesList list={this.state.series}/>
       </div>
     )
   }
